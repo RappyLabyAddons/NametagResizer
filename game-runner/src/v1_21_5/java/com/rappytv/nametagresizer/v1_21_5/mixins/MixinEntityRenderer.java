430bb5b18bedaedc708d9d91b7b856d503ff7ebd
RappyTV <rappytv@rappytv.com>
2025-05-21 15:46:18 +0200

    Add 1.21.x implementations

---
+++ game-runner/src/v1_21_5/java/com/rappytv/nametagresizer/v1_21_5/mixins/MixinEntityRenderer.java
@@ -0,0 +1,36 @@
+package com.rappytv.nametagresizer.v1_21_5.mixins;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.rappytv.nametagresizer.event.NametagSizeEvent;
+import net.labymod.api.Laby;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.client.renderer.entity.state.EntityRenderState;
+import net.minecraft.client.renderer.entity.state.PlayerRenderState;
+import net.minecraft.network.chat.Component;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(EntityRenderer.class)
+public class MixinEntityRenderer<S extends EntityRenderState> {
+
+  @Unique
+  private boolean sandbox$isPlayer;
+
+  @Inject(method = "renderNameTag", at = @At("HEAD"))
+  public void renderNameTag(S state, Component component, PoseStack stack,
+      MultiBufferSource bufferSource, int $$4, CallbackInfo ci) {
+    this.sandbox$isPlayer = state instanceof PlayerRenderState;
+  }
+
+  @Redirect(method = "renderNameTag", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;scale(FFF)V"))
+  public void scale(PoseStack stack, float x, float y, float z) {
+    NametagSizeEvent event = new NametagSizeEvent(this.sandbox$isPlayer, x, y, z);
+    Laby.fireEvent(event);
+    stack.scale(event.getX(), event.getY(), event.getZ());
+  }
+}
