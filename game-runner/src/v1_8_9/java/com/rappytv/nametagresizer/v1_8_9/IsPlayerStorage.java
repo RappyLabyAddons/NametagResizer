233f93123667520741acf0f976c4b15739ebf347
RappyTV <rappytv@rappytv.com>
2025-05-21 20:20:44 +0200

    Add 1.8.9 implementation

---
+++ game-runner/src/v1_8_9/java/com/rappytv/nametagresizer/v1_8_9/IsPlayerStorage.java
@@ -0,0 +1,14 @@
+package com.rappytv.nametagresizer.v1_8_9;
+
+public class IsPlayerStorage {
+
+    private static boolean isPlayer = false;
+
+    public static boolean isPlayer() {
+        return isPlayer;
+    }
+
+    public static void setPlayer(boolean player) {
+        isPlayer = player;
+    }
+}
---
+++ game-runner/src/v1_8_9/java/com/rappytv/nametagresizer/v1_8_9/mixins/MixinRender.java
@@ -0,0 +1,34 @@
+package com.rappytv.nametagresizer.v1_8_9.mixins;
+
+import com.rappytv.nametagresizer.event.NametagSizeEvent;
+import com.rappytv.nametagresizer.v1_8_9.IsPlayerStorage;
+import net.labymod.api.Laby;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.entity.Render;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = Render.class, priority = 1001)
+public class MixinRender<T extends Entity> {
+
+  @Inject(method = "renderLivingLabel", at = @At("HEAD"))
+  public void renderNameTag(T entity, String lvt_2_1_, double lvt_3_1_, double lvt_5_1_,
+      double lvt_7_1_, int lvt_9_1_, CallbackInfo ci) {
+    IsPlayerStorage.setPlayer(entity instanceof EntityPlayer);
+  }
+
+  @Redirect(
+      method = "renderLivingLabel",
+      at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/GlStateManager;scale(FFF)V")
+  )
+  private void scale(float x, float y, float z) {
+    NametagSizeEvent event = new NametagSizeEvent(IsPlayerStorage.isPlayer(), x, y, z);
+    Laby.fireEvent(event);
+    GlStateManager.scale(event.getX(), event.getY(), event.getZ());
+  }
+}
