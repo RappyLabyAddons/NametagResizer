71d40bb7a4b4bdfbbc196d7a885af4b2eaf9139f
RappyTV <rappytv@rappytv.com>
2025-10-10 17:13:50 +0200

    chore: Register and support version 1.21.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ labyMod {
         displayName = "Nametag Resizer"
         author = "RappyTV"
         description = "Easily edit the render size of nametags"
-        minecraftVersion = "1.8.9<1.21.9"
+        minecraftVersion = "1.8.9<1.21.10"
         version = rootProject.version.toString()
     }
 
