80b5fc48e4603c7c5a8189ac1fa06bc7aa672765
RappyTV <rappytv@rappytv.com>
2026-02-15 10:46:46 +0100

    feat: Add 1.21.11 implementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ labyMod {
         displayName = "Nametag Resizer"
         author = "RappyTV"
         description = "Easily edit the render size of nametags"
-        minecraftVersion = "1.8.9<1.21.10"
+        minecraftVersion = "1.8.9<1.21.11"
         version = rootProject.version.toString()
     }
 
---
+++ game-runner/src/v1_21_11/java/com/rappytv/nametagresizer/v1_21_11/mixins/MixinAvatarRenderer.java
@@ -0,0 +1,25 @@
+package com.rappytv.nametagresizer.v1_21_11.mixins;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.rappytv.nametagresizer.v1_21_11.RenderStateAccessor;
+import net.minecraft.client.renderer.SubmitNodeCollector;
+import net.minecraft.client.renderer.entity.player.AvatarRenderer;
+import net.minecraft.client.renderer.entity.state.AvatarRenderState;
+import net.minecraft.client.renderer.state.CameraRenderState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(AvatarRenderer.class)
+public class MixinAvatarRenderer {
+
+  @Inject(
+      method = "submitNameTag(Lnet/minecraft/client/renderer/entity/state/AvatarRenderState;Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/SubmitNodeCollector;Lnet/minecraft/client/renderer/state/CameraRenderState;)V",
+      at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/SubmitNodeCollector;submitNameTag(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/world/phys/Vec3;ILnet/minecraft/network/chat/Component;ZIDLnet/minecraft/client/renderer/state/CameraRenderState;)V")
+  )
+  private void submitNameTag(AvatarRenderState $$0, PoseStack $$1, SubmitNodeCollector $$2,
+      CameraRenderState $$3, CallbackInfo ci) {
+    RenderStateAccessor.setPlayer(true);
+  }
+}
